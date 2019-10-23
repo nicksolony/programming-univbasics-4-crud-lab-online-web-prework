4d658a9cebbc98561b405fe6d0e03422058cb0cf
Nick Solonyy <nicksolony@gmail.com>
2019-10-23 02:35:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,9 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array=["Nick","Anna","Sasha"]
-  element="Lena"
-  array<<element
+array << element
 end
 
 def add_element_to_start_of_array(array, element)
